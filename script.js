f94c213649ef5fc097104f176cc960412cd57b2d
EmeraldAbigail <151237159+EmeraldAbigail@users.noreply.github.com>
2023-12-21 12:19:48 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let count1 = 9; 
+let countElement1 = document.querySelector("#count1");
+
+function add1() {
+    count1++;
+    countElement1.innerText = count1 + "like(s)";
+
+}
+let count2 = 12;
+let countElement2 = document.querySelector ("#count2");
+
+function add2() {
+    count2++;
+    countElement2.innerText = count2 + "like(s)"; 
+}
+let count3 = 9; 
+let countElement3 = document.querySelector("#count3");
+
+function add3() {
+    count3++;
+    countElement3.innerText = count3 + "like(s)";
+}
